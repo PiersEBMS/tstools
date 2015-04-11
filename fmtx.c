4879116b50914fb2fac2a8acf8d66b1adb9140a4
Piers EBMS <piers.scannell@ericsson.com>
2015-04-11 14:40:58 +0100

    update HMS format (millseconds) fixes #22

--- fmtx.c
+++ fmtx.c
@@ -93,7 +93,7 @@ const TCHAR *fmtx_timestamp(int64_t n, unsigned int flags)
       a27 /= I64K(60);
       m = (unsigned int)(a27 % I64K(60));
       h = (unsigned int)(a27 / I64K(60));
-      _sntprintf(buf, FMTX_BUFFER_SIZE, _T("%s%u:%02u:%02u.%04u"), n27 < 0 ? _T("-") : _T(""), h, m, s, f/1000);
+      _sntprintf(buf, FMTX_BUFFER_SIZE, _T("%s%u:%02u:%02u.%03u"), n27 < 0 ? _T("-") : _T(""), h, m, s, f/1000);
       break;
     }
 
